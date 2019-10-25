4a2825f2bdc099c69714fa706853e4877050ac71
Abdi <oxabdi75@gmail.com>
2019-10-25 15:57:24 +0300

    added variable in js a function

---
+++ js/main.js
@@ -0,0 +1,13 @@
+//Declare arrays
+
+var maleAkanGhana = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "KOfi", "Kwame"];
+var femaleAkanGhana = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaw", "Afua", "Ama"];
+var daysOfTheWeel = ["Sunday","Monday","Tuesday","Wensday","Thursday","Friday","Saturday"];
+ 
+//Hide bootstrap button
+
+document.getElementById("display").style.display ="none"
+
+//called when submit button is clicked
+
+function onClick()
