35411090179d238ceb36ab6ab0deb81d28d6f94f
Abdi <oxabdi75@gmail.com>
2019-10-25 16:35:14 +0300

    added a div with alert and id display

--- js/main.js
+++ js/main.js
@@ -1,13 +1,43 @@
 //Declare arrays
 
-var maleAkanGhana = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "KOfi", "Kwame"];
-var femaleAkanGhana = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaw", "Afua", "Ama"];
-var daysOfTheWeel = ["Sunday","Monday","Tuesday","Wensday","Thursday","Friday","Saturday"];
- 
+var maleAkanGhana = [
+  "Kwasi",
+  "Kwadwo",
+  "Kwabena",
+  "Kwaku",
+  "Yaw",
+  "KOfi",
+  "Kwame"
+];
+var femaleAkanGhana = [
+  "Akosua",
+  "Adwoa",
+  "Abenaa",
+  "Akua",
+  "Yaw",
+  "Afua",
+  "Ama"
+];
+var daysOfTheWeek = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wensday",
+  "Thursday",
+  "Friday",
+  "Saturday"
+];
+
 //Hide bootstrap button
 
-document.getElementById("display").style.display ="none"
+document.getElementById("display").style.display = "none";
 
 //called when submit button is clicked
 
-function onClick()
+function display() {
+  var day = daysOfTheWeek[calculateDay()];
+  var name = getAkanGhana();
+  document.getElementById("day").innerHTML = day;
+  document.getElementById("name").innerHTML = name;
+  document.getElementById("display").style.display = "block";
+}
