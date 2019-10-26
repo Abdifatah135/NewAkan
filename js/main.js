7f540c41f584aa9729fbdd4c18e58d6fff2de0cd
Abdi <oxabdi75@gmail.com>
2019-10-26 09:06:38 +0300

    added array in my js

--- js/main.js
+++ js/main.js
@@ -5,4 +5,14 @@ function validation() {
   var yy = parseInt(month);
   var year = document.getElementById("year").value;
   var yy = parseInt(year);
-}
+  var cc =(yy-1)/100+1;
+
+  var result = parseInt(((cc / 4) - 2 * cc - 1) + ((5 * yy / 4)) + ((26 * (mm + 1) / 10)) + dd) % 7;
+
+  // declaring arrays
+
+        var maleAkan = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+        var femaleAkan = ["Akosua", " Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+        if (document.getElementById("gender").checked) {
+          var gender = 'male';}
+
