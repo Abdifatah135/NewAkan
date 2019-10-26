3df144f71c7838fbc90402ee6bbdfe628510d994
Abdi <oxabdi75@gmail.com>
2019-10-26 08:43:48 +0300

    added a function in my js script

---
+++ js/main.js
@@ -0,0 +1,8 @@
+function validation() {
+  var date = document.getElementById("day").Value;
+  var yy = parseInt(day);
+  var month = document.getElementById("month").value;
+  var yy = parseInt(month);
+  var year = document.getElementById("year").value;
+  var yy = parseInt(year);
+}
